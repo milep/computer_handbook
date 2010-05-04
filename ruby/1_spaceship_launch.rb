dfa645b7fcf4d25a9de29de734cdc500425d65a8
Miika Leppänen <milep@mlemacbook.local>
2010-05-04 20:47:52 +0300

    Added some comments

--- ruby/1_spaceship_launch.rb
+++ ruby/1_spaceship_launch.rb
@@ -1,4 +1,5 @@
 puts "SPACESHIP LAUNCH"
+#rand(x) returns random number between 0 and x - 1
 gravity = rand(20) + 1
 weight = rand(40) + 1
 resistance = gravity * weight
@@ -8,13 +9,13 @@ puts "Give thrust power:"
 
 try_count = 0
 
-while try_count < 10
+while try_count < 10 #loop while try_count is less than 10
   answer = gets.chomp
   thrust = answer.to_i
 
   if thrust == resistance
     puts "Launch was successfull!"
-    break
+    break #break from loop, program continues from the next line after the loop
   elsif thrust > resistance
     puts "Too much thrust power"
   else
@@ -30,3 +31,5 @@ while try_count < 10
   
   try_count = try_count + 1
 end
+#program jumps here, if the break is called in the loop above
+#or if the while condition is not true
